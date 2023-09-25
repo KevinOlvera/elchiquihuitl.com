26c33c7cab101e47cbc2994e6b196e4bd2d587d3
Kevin Olvera <kevin.jesus.olvera@gmail.com>
2023-09-25 17:24:20 -0600

    Revert "Borrar Slider"
    This reverts commit ad1cc781782895e88e83eb0e37805dd972857a19.

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,14 @@ function App() {
           </div>
         </div>
       </nav>
+      <Slider
+        images={[
+          'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3099&q=80',
+          'https://images.unsplash.com/photo-1550547660-d9450f859349?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2097&q=80',
+          'https://images.unsplash.com/photo-1551782450-a2132b4ba21d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3269&q=80',
+          'https://images.unsplash.com/photo-1596649299486-4cdea56fd59d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80'
+        ]}
+      />
       <CategoriesPanel />
       <div className="p-6">
         <button
