2122f91ef5d3826fafc1ffe30f6e87c7afbf66ec
Kevin Olvera <kevin.jesus.olvera@gmail.com>
2023-09-30 00:25:36 -0600

    Added skeleton and split Components

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,157 +1,107 @@
 /* import { useCartItemActions } from '../hooks/useCartItemActions' */
 
-import { Button, Image } from '@nextui-org/react'
+import { Button, Image, Skeleton } from '@nextui-org/react'
 
-import { ClockIcon, FireIcon, MinusIcon, PlusIcon, StartIcon, TrashIcon } from './common/Icons'
+import { ClockIcon, FireIcon, PencilSquareIcon, PlusIcon, StartIcon } from './common/Icons'
 import PriceText from './common/PriceText'
 import { type MenuItem } from '../types'
+import { useState } from 'react'
+import QuantityControl from './common/QuantityControl'
 
 interface ItemProps {
   data: MenuItem
-  inCart: boolean
   quantity: number
+  onAdd: () => void
+  onRemove: () => void
 }
 
 function Item(props: ItemProps) {
-  /*   const { addCartItem } = useCartItemActions() */
+  const [isLoaded, setIsLoaded] = useState(false)
+
   return (
     <div
       className="flex flex-col space-y-4"
     >
       <div className="flex space-x-4">
-        <Image
-          alt={props.data.name}
-          /* Bordered or non */
-          className="object-cover object-center border border-gray-200 dark:border-gray-500"
-          width={96}
-          height={96}
-          src={props.data.image}
-          isBlurred={false}
-        />
+        <Skeleton isLoaded={isLoaded} className='rounded-xl'>
+          <Image
+            alt={props.data.name}
+            /* Bordered or non */
+            className="object-cover object-center border border-gray-200 dark:border-gray-500"
+            width={96}
+            height={96}
+            src={props.data.image}
+            isBlurred={false}
+            onLoad={() => { setIsLoaded(true) }}
+          />
+        </Skeleton>
         <div className='flex flex-col w-full space-y-2'>
           <div className='flex justify-between'>
-            <span className='text-sm font-semibold text-gray-500 dark:text-gray-200'>{props.data.name}</span>
+            <Skeleton isLoaded={isLoaded} className='rounded-md'>
+              <span className='text-sm font-semibold text-gray-500 dark:text-gray-200'>{props.data.name}</span>
+            </Skeleton>
             {
               (props.data.time != null)
-                ? <span className='flex items-center justify-end text-sm text-gray-400'>
-                  <ClockIcon className='w-4 h-4 mr-1' /> {props.data.time} min
-                </span>
+                ? <div className='flex items-start justify-end w-20'>
+                  <Skeleton isLoaded={isLoaded} className='rounded-md'>
+                    <span className='flex items-center justify-end text-sm text-gray-400'>
+                      <ClockIcon className='w-4 h-4 mr-1' /> {props.data.time} min
+                    </span>
+                  </Skeleton>
+                </div>
                 : ''
             }
           </div>
           <span className='text-sm text-gray-500 line-clamp-3'>
-            {props.data.description}
+            <Skeleton isLoaded={isLoaded} className='rounded-md'>
+              {props.data.description}
+            </Skeleton>
           </span>
         </div>
       </div>
-      <div className="flex justify-between items-center">
-        <PriceText
-          value={props.data.price}
-          currency='MXN'
-        />
-        <span className='flex text-sm items-center text-gray-500'>
-          <StartIcon className='w-4 h-4 mr-1 text-warning-500' /> 4.5
-        </span>
-        <span className='flex text-sm items-center text-gray-500'>
-          <FireIcon className='w-4 h-4 mr-1 text-danger-500' /> 86 kcal
-        </span>
-        <div className='flex items-center'>
-          <Button
-            variant='light'
-            size='sm'
-            isIconOnly
-          >
-            <MinusIcon className='w-4 h-4' />
-          </Button>
-          <span className="text-gray-600 dark:text-white w-10 text-center">
-            {props.quantity}
+      <Skeleton isLoaded={isLoaded} className='rounded-md'>
+        <div className="flex justify-between items-center">
+          <PriceText
+            value={props.data.price}
+            currency='MXN'
+          />
+          <span className='flex text-sm items-center text-gray-500'>
+            <StartIcon className='w-4 h-4 mr-1 text-warning-500' /> 4.5
           </span>
-          <Button
-            variant='light'
-            size='sm'
-            isIconOnly
-          >
-            <PlusIcon className='w-4 h-4' />
-          </Button>
-        </div>
-      </div>
-      {/* <img
-        className="w-24 h-24 rounded-xl object-cover object-center border border-gray-200"
-        src={props.data.image}
-        style={{
-          pointerEvents: 'none'
-        }}
-      />
-      <div
-        className='flex space-x-4 justify-between w-full'
-      >
-        <div
-          className='flex flex-col space-y-2'
-        >
-          <span className="text-sm font-semibold text-gray-500">
-            {props.data.name}
+          <span className='flex text-sm items-center text-gray-500'>
+            <FireIcon className='w-4 h-4 mr-1 text-danger-500' /> 86 kcal
           </span>
-          <span className="text-sm text-gray-500 line-clamp-3">
-            {props.data.description}
-          </span>
-        </div>
-        <div
-          className='flex flex-col items-right justify-center space-y-2'
-        >
           {
-            props.inCart
-              ? <>
-                {
-                  (props.data.time != null)
-                    ? <span className='flex items-center justify-end text-sm text-gray-400'>
-                      <ClockIcon className='w-4 h-4 mr-1' /> {props.data.time} min
-                    </span>
-                    : ''
-                }
-                <PriceText
-                  value={props.data.price}
-                  currency='MXN'
-                  className='text-right'
-                />
-                <div
-                  className='flex items-center justify-center space-x-2'
-                >
-                  <Button
-                    variant='light'
-                    size='sm'
-                    isIconOnly
-                  >
-                    <MinusIcon className='w-4 h-4' />
-                  </Button>
-                  <span className="text-gray-600">
-                    {props.quantity}
-                  </span>
-                  <Button
-                    variant='light'
-                    size='sm'
-                    isIconOnly
-                  >
-                    <PlusIcon className='w-4 h-4' />
-                  </Button>
-                </div>
-              </>
-              : <>
+            (props.quantity === 0)
+              ? <div className='flex items-center space-x-2'>
                 <Button
+                  isIconOnly
+                  variant='flat'
+                  size='sm'
+                  className='hidden'
+                >
+                  <PencilSquareIcon className='w-4 h-4' />
+                </Button>
+                <Button
+                  isIconOnly
                   color='primary'
                   variant='flat'
                   size='sm'
-                  isIconOnly
+                  onClick={() => { props.onAdd() }}
                 >
-                  <PlusIcon />
+                  <PlusIcon className='w-4 h-4' />
                 </Button>
-                <PriceText
-                  value={props.data.price}
-                  currency='MXN'
+              </div>
+              : <div className='flex items-center'>
+                <QuantityControl
+                  quantity={props.quantity}
+                  onAdd={() => { props.onAdd() }}
+                  onSub={() => { props.onRemove() }}
                 />
-              </>
+              </div>
           }
         </div>
-      </div> */}
+      </Skeleton>
     </div>
   )
 }
---
+++ src/components/common/QuantityControl.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@nextui-org/react'
+import { MinusIcon, PlusIcon } from './Icons'
+
+interface QuantityControlProps {
+  quantity: number
+  onAdd: () => void
+  onSub: () => void
+}
+
+function QuantityControl(props: QuantityControlProps) {
+  return (
+    <>
+      <Button
+        variant='light'
+        size='sm'
+        isIconOnly
+        onClick={() => { props.onSub() }}
+      >
+        <MinusIcon className='w-4 h-4' />
+      </Button>
+      <span className="text-gray-600 dark:text-white w-10 text-center">
+        {props.quantity}
+      </span>
+      <Button
+        variant='light'
+        size='sm'
+        isIconOnly
+        onClick={() => { props.onAdd() }}
+      >
+        <PlusIcon className='w-4 h-4' />
+      </Button>
+    </>
+  )
+}
+
+export default QuantityControl
